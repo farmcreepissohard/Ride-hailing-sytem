11b450ae5c75fc4be31d23f927672d79076b57c6
Lam Hong Anh Duc <neyduc167@gmail.com>
2026-08-20 15:12:25 +0700

    feat: create trip database infrastructure: - build trip database schema by using migration from flyway - using hibernate jpa to validate instead of ORM function

---
+++ trip-service/src/main/resources/db/migration/V1__create_trips.sql
@@ -0,0 +1,27 @@
+CREATE TABLE trips(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
+    customer_id UUID NOT NULL,
+    driver_id UUID,
+
+    pickup_location TEXT NOT NULL,
+    dropoff_location TEXT NOT NULL,
+    pickup_lat DECIMAL(10,8) NOT NULL,
+    pickup_lng DECIMAL(11,8) NOT NULL,
+    dropoff_lat DECIMAL(10,8) NOT NULL,
+    dropoff_lng DECIMAL(11,8) NOT NULL,
+    
+    distance DOUBLE PRECISION NOT NULL,
+    
+    total_amount DOUBLE PRECISION NOT NULL,
+    driver_earning DOUBLE PRECISION NOT NULL,
+    
+    trip_status VARCHAR(20) NOT NULL DEFAULT 'PENDING',
+    
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    accepted_at TIMESTAMPTZ,
+    pickedup_at TIMESTAMPTZ,
+    completed_at TIMESTAMPTZ,
+    cancelled_at TIMESTAMPTZ,
+    
+    CONSTRAINT check_status CHECK (trip_status IN ('PENDING', 'ACCEPTED', 'ON TRIP', 'COMPLETED', 'CANCELLED'))
+);
